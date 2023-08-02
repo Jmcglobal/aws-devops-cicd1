101e66d97ea8dd5e1991d7f9129de7652938f3f1
Mmadubugwu Chibuife <Mmadubugwuchibuife@gmail.com>
2023-08-02 14:20:42 +0100

    run test

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers"
-pkill node
+lsof -i:3000 | grep LISTEN >/dev/null
+status=$?  # Removed the unnecessary `echo` and backticks here
+if [ "$status" -eq 0 ]; then  # Corrected the comparison operator here
+    pid=`lsof -i:3000 | grep LISTEN | awk -F " " '{ print $2 }'`  # Also used $() instead of backticks for command substitution
+    kill $pid
+else
+  echo "No process is running"
+fi
